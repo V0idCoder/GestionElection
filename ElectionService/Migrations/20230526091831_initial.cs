3abb105daaa09fa6490ac57805f15ed666a51c22
V0idCoder <paulperhaita.ch@gmail.com>
2023-06-02 15:25:21 +0200

    TreeView

---
+++ ElectionService/Migrations/20230526091831_initial.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElectionService.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candidats",
+                columns: table => new
+                {
+                    CandidatId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Numero = table.Column<int>(type: "int", nullable: false),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Prenom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateNaissance = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Profession = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ListeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidats", x => x.CandidatId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Communes",
+                columns: table => new
+                {
+                    communeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    npa = table.Column<int>(type: "int", nullable: false),
+                    numero = table.Column<int>(type: "int", nullable: false),
+                    canton = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Communes", x => x.communeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Listes",
+                columns: table => new
+                {
+                    ListeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Numero = table.Column<int>(type: "int", nullable: false),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Abreviation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NbBulletinCompact = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Listes", x => x.ListeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Suffrages",
+                columns: table => new
+                {
+                    suffrageId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nbVote = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suffrages", x => x.suffrageId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Elections",
+                columns: table => new
+                {
+                    ElectionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateElection = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NbSiege = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NbBulletin = table.Column<int>(type: "int", nullable: false),
+                    NbBulletinNull = table.Column<int>(type: "int", nullable: false),
+                    NbBulletinBlanc = table.Column<int>(type: "int", nullable: false),
+                    CommuneId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Elections", x => x.ElectionId);
+                    table.ForeignKey(
+                        name: "FK_Elections_Communes_CommuneId",
+                        column: x => x.CommuneId,
+                        principalTable: "Communes",
+                        principalColumn: "communeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bulletins",
+                columns: table => new
+                {
+                    BulletinId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Numero = table.Column<int>(type: "int", nullable: false),
+                    ListeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bulletins", x => x.BulletinId);
+                    table.ForeignKey(
+                        name: "FK_Bulletins_Listes_ListeId",
+                        column: x => x.ListeId,
+                        principalTable: "Listes",
+                        principalColumn: "ListeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bulletins_ListeId",
+                table: "Bulletins",
+                column: "ListeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Elections_CommuneId",
+                table: "Elections",
+                column: "CommuneId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bulletins");
+
+            migrationBuilder.DropTable(
+                name: "Candidats");
+
+            migrationBuilder.DropTable(
+                name: "Elections");
+
+            migrationBuilder.DropTable(
+                name: "Suffrages");
+
+            migrationBuilder.DropTable(
+                name: "Listes");
+
+            migrationBuilder.DropTable(
+                name: "Communes");
+        }
+    }
+}
---
+++ ElectionService/Model/ElectionContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ElectionService.Model
+{
+    public class ElectionContext : DbContext 
+    {
+        public ElectionContext(DbContextOptions<ElectionContext> options): base(options){}
+        public DbSet<Bulletin>Bulletins { get; set; }
+        public DbSet<Candidat> Candidats { get; set; }
+        public DbSet<Commune> Communes { get; set; }
+        public DbSet<Election> Elections { get; set; }
+        public DbSet<Liste> Listes { get; set; }
+        public DbSet<Suffrage> Suffrages { get; set; }
+    }
+}
