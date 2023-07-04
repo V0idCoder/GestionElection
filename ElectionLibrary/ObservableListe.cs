8daab7f1aa6c775bb442a2d9b7860630f3d6ed3a
cp-21ega <cp-21ega@PC-BD83-11>
2023-07-04 09:33:36 +0200

    MVVM (ObservableClass)

---
+++ ElectionLibrary/ObservableListe.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionLibrary
+{
+    public class ObservableListe : ObservableObject
+    {
+        private readonly Liste liste;
+
+        public ObservableListe(Liste liste) => this.liste = liste;
+
+        public int Numero
+        {
+            get => liste.Numero;
+            set => SetProperty(liste.Numero, value, liste, (u, n) => u.Numero = n);
+        }
+
+        public string Nom
+        {
+            get => liste.Nom;
+            set => SetProperty(liste.Nom, value, liste, (u, n) => u.Nom = n);
+        }
+        public string Abreviation
+        {
+            get => liste.Abreviation;
+            set => SetProperty(liste.Abreviation, value, liste, (u, n) => u.Abreviation = n);
+        }
+        public List<Candidat> Candidats
+        {
+            get => liste.Candidats;
+            set => SetProperty(liste.Candidats, value, liste, (u, n) => u.Candidats = n);
+        }
+    }
+}
--- GestionElectionUI/MainWindow.xaml.cs
+++ GestionElectionUI/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ElectionLibrary;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +22,53 @@ namespace GestionElectionUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Commune> Communes = new ObservableCollection<Commune>();
         public MainWindow()
         {
             InitializeComponent();
+            InitialiteTestData();
+            MainTreeView.ItemsSource = Communes;
+
         }
 
         private void TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
+            if (MainTreeView.SelectedItem is Election)
+            {
+                var election = (Election)MainTreeView.SelectedItem;
+                var dg = new DataGrid();
+                var lst = new List<Liste>(election.Listes);
+                dg.ItemsSource = lst;
+                ScrollPanel.Content = dg;
+            }
+            
+            //if(CommuneItem.IsSelected)
+            //{
+            //    Election.IsEnabled = false;
+            //    Commune.IsEnabled = true;
+            //} else 
+            //{
+            //    Commune.IsEnabled = false;
+            //    Election.IsEnabled = true;
+            //}
+        }
 
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        void InitialiteTestData()
+        {
+            var stimier = new Commune() { Nom = "St-Imier", Elections = new List<Election>()};
+            Communes.Add(stimier);
+            var ele1 = new Election() { Nom = "Conseil communale 2023", Listes = new List<Liste>()};
+            stimier.Elections.Add(ele1);
+            var lst1 = new Liste() { Nom = "UDC" };
+            var lst2 = new Liste() { Nom = "PLR" };
+            var lst3 = new Liste() { Nom = "PS" };
+            ele1.Listes.Add(lst1);
+            ele1.Listes.Add(lst2);
         }
     }
 }
